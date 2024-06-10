3e642a024bdc7f633bc56a3341087b3a5cab9f11
pandaman <fanda.alvaridzi01@gmail.com>
2024-06-10 11:37:31 +0700

    Set up bottom tab navigation

---
+++ src/screens/Favorite.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Favorite = ():JSX.Element => {
+  return (
+    <View>
+        <Text>Favorite</Text>
+    </View>
+  )
+}
+
+export default Favorite
